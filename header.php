6821fbf09d1f397c22c81c416e56e1602b612fa9
Equivalentideas <luke@equivalentideas.com>
2013-08-08 14:04:14 +1000

    base content

--- header.php
+++ header.php
@@ -1,6 +1,16 @@
 <header>
+	<img alt="Collagraph web design logo" />
 	<h1>Collagraph</h1>
 
 	<p>Clear design for the web</p>
 	
 </header>
+
+	
+<p>Collagraph is a web design and content strategy partnership based in Sydney, Australia.</p>
+
+	
+<ul>
+	<li>Email: info@collagrph.com.au</li>
+	<li>Phone Luke: +61 406566693</li>
+</ul>
--- index.php
+++ index.php
@@ -10,4 +10,41 @@
 	
 -->
 
+<!-- What we do -->
+
+<section id="what-we-do">
+	<h2>What we do</h2>
+	
+	<h3>Designed for you</h3>
+	
+	<p>A website designed from the ground up to achieve <em>your</em> goals. We make sure you, and your users, are happy.</p>
+	
+	<h3>Content for the web</h3>
+	
+	<p>We help you to develop strong web content that&#8217;s clear and effective. Well organised information puts you in control.</p>
+	
+	<h3>Mobile, Tablet, Desktop, ?</h3>
+	
+	<p>One site that looks and works great no matter how your users view it.</p>
+</section>
+
+<section id="our-work">
+	<h2>Our work</h2>
+
+	<h3>The Plan</h3>
+	
+	<h3>Marawa The Amazing</h3>
+	
+	<h3>Investigate.org.au</h3>
+
+</section>
+
+<section id="about-us">
+	<h2>Who we are</h2>
+	
+	<h3>Robert Irish</h3>
+	
+	<h3>Luke Bacon</h3>
+</section>
+
 <?php require_once('footer.php'); ?>
