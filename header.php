0f6da30302b75fa498ba515e48a662dbc8c48af2
Equivalentideas <luke@equivalentideas.com>
2013-08-09 13:33:14 +1000

    added micro formats to head

--- header.php
+++ header.php
@@ -1,14 +1,14 @@
-<header>
+<header class="h-card">
 
-	<img class="logo" src="images/logo.png" alt="Collagraph logo" />
+	<img class="logo u-logo" src="images/logo.png" alt="Collagraph logo" />
 
-	<h1 class="logo">Collagraph</h1>
+	<h1 class="logo p-name">Collagraph</h1>
 
-	<p class="distinct">Clear&nbsp;design&nbsp;for&nbsp;the&nbsp;web, based&nbsp;in&nbsp;Sydney,&nbsp;Australia.</p>
+	<p class="distinct lead p-role">Clear&nbsp;design for the web, <span class="p-adr">based in <span class="p-locality">Sydney</span>, <span class="p-country-name">Australia</span>.</span></p>
 	
-	<ul>
-		<li>Email: <a href="mailto:info@collagraph.com.au">info@collagraph.com.au</a></li>
-		<li>Phone: <a href="tel:+61406566693">+61 406 566 693</a></li>
+	<ul class="details">
+		<li>Email: <a href="mailto:info@collagraph.com.au" class="u-email">info@collagraph.com.au</a></li>
+		<li>Phone: <a href="tel:+61406566693" class="p-tel">+61 406 566 693</a></li>
 	</ul>
 	
 </header>
