74eddd2634fb8e49f711673837bdc6cd42791229
Equivalentideas <luke@equivalentideas.com>
2013-08-09 10:40:20 +1000

    removed the National img and added, logo class

--- header.php
+++ header.php
@@ -1,11 +1,7 @@
 <header>
 	<img alt="" />
-	<h1 class="visuallyhidden">Collagraph</h1>
+	<h1 class="logo">Collagraph</h1>
 	
-	<figure class="logo">
-		<img src="images/logo.png" title="Collagraph" alt="Collagraph" />
-	</figure>
-
 	<p>Clear design for the web, based in Sydney, Australia.</p>
 	
 </header>
