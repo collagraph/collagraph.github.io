91d01bf1a1d5c35cdd66f9c404ad1a815007a241
Robert Irish <robert.f.irish@gmail.com>
2013-08-09 12:45:24 +1000

    move email and phone list inside of <header> tag

--- header.php
+++ header.php
@@ -4,12 +4,11 @@
 
 	<h1 class="logo">Collagraph</h1>
 
-	<p>Clear design for the web, based in Sydney, Australia.</p>
 	<p class="distinct">Clear&nbsp;design&nbsp;for&nbsp;the&nbsp;web, based&nbsp;in&nbsp;Sydney,&nbsp;Australia.</p>
 	
-</header>
+	<ul>
+		<li>Email: <a href="mailto:info@collagraph.com.au">info@collagraph.com.au</a></li>
+		<li>Phone: <a href="tel:+61406566693">+61 406 566 693</a></li>
+	</ul>
 	
-<ul>
-	<li>Email: <a href="mailto:info@collagraph.com.au">info@collagrph.com.au</a></li>
-	<li>Phone: <a href="tel:+61406566693">+61 406 566 693</a></li>
-</ul>
+</header>
