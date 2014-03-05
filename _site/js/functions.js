9392966e65dbc70a162631321e1e67887023c1e8
Robert Irish <robert.f.irish@gmail.com>
2014-03-06 08:49:13 +1100

    update style.css index.html functions.js

--- _site/js/functions.js
+++ _site/js/functions.js
@@ -1,9 +1,12 @@
-// remap jQuery to $
-(function($){
+/* -------------------------------------------------------------------------------
+ * functions
+ * -------------------------------------------------------------------------------*/
 
+(function($){
     $('fieldset:not(#foo) #phone-q').click(function() {
         if($('#phone-q').is(':checked')) {
             $('#phone-c').removeClass('checkbox-focused').addClass('checkbox-checked-focused');
+
             $('#more-phone').addClass('display-more').removeClass('display-hide');
         } else {
             $('#phone-c').removeClass('checkbox-checked, checkbox-checked-focused').addClass('checkbox-focused');
@@ -14,23 +17,28 @@
     $('fieldset:not(#foo) #deadline-q').click(function() {
         if($('#deadline-q').is(':checked')) {
             $('#deadline-c').removeClass('checkbox-focused').addClass('checkbox-checked-focused');
+
             $('#more-deadline').addClass('display-more').removeClass('display-hide');
+
             $('#deadline').focus();
         } else {
             $('#deadline-c').removeClass('checkbox-checked, checkbox-checked-focused').addClass('checkbox-focused');
+
             $('#more-deadline').removeClass('display-more').addClass('display-hide');
         }
     });
 
-	$('fieldset:not(#foo) #budget-q').click(function() {
-		if($('#budget-q').is(':checked')) {
-			$('#budget-c').removeClass('checkbox-focused').addClass('checkbox-checked-focused');
-			$('#more-budget').addClass('display-more').removeClass('display-hide');
-		} else {
-			$('#budget-c').removeClass('checkbox-checked, checkbox-checked-focused').addClass('checkbox-focused');
-			$('#more-budget').removeClass('display-more').addClass('display-hide');
-		}
-	});
+    $('fieldset:not(#foo) #budget-q').click(function() {
+        if($('#budget-q').is(':checked')) {
+            $('#budget-c').removeClass('checkbox-focused').addClass('checkbox-checked-focused');
+
+            $('#more-budget').addClass('display-more').removeClass('display-hide');
+        } else {
+            $('#budget-c').removeClass('checkbox-checked, checkbox-checked-focused').addClass('checkbox-focused');
+
+            $('#more-budget').removeClass('display-more').addClass('display-hide');
+        }
+    });
 
     $('fieldset:not(#foo) #phone-q').focus(function() {
         if ($('#phone-q').is(':checked')) {
@@ -74,45 +82,7 @@
         }
     });
 
-    // flexslider slide changer
-	$('.slide-1').click(function(){
-		var slidePath=$(this).closest('article').attr('id');
-		var slideURL='/assets/portfolio/'+slidePath+'/slide-1.jpg';
-        $('#'+slidePath+' .photo img').attr('src',slideURL);
-		$('#'+slidePath+' .slide-link').removeClass('current');
-		$(this).addClass('current');
-	});
-
-	$('.slide-2').click(function(){
-		var slidePath=$(this).closest('article').attr('id');
-		var slideURL='/assets/portfolio/'+slidePath+'/slide-2.jpg';
-		$('#'+slidePath+' .photo img').attr('src',slideURL);
-		$('#'+slidePath+' .slide-link').removeClass('current');
-		$(this).addClass('current');
-	});
-
-	$('.slide-3').click(function(){
-		var slidePath=$(this).closest('article').attr('id');
-		var slideURL='/assets/portfolio/'+slidePath+'/slide-3.jpg';
-        $('#'+slidePath+' .photo img').attr('src',slideURL);
-		$('#'+slidePath+' .slide-link').removeClass('current');
-		$(this).addClass('current');
-	});
-
     $(document).ready(function() {
-
-		// // flexslider
-		// $(".flexslider")
-		// .flexslider({
-		// 	animation: "slide",
-		// 	slideshow: false,
-		// 	useCSS: true,
-		// 	animationLoop: false,
-		// 	smoothHeight: false,
-  //           directionNav: false,
-		// 	controlNav: false
-		// }).flexsliderManualDirectionControls();
-
         // smooth scrolling
         $('.scroll').click(function(event){
             event.preventDefault();
@@ -123,6 +93,7 @@
 
     });
 
+    // preload images
     function preload(arrayOfImages) {
         $(arrayOfImages).each(function(){
             $('<img/>')[0].src = this;
@@ -131,41 +102,29 @@
 
     $(window).bind("load", function() {
         preload([
-            '/assets/portfolio/project-1/slide-2.jpg',
-            '/assets/portfolio/project-1/slide-3.jpg',
-            '/assets/phone-icon-blue-retina.png',
-            '/assets/tablet-icon-retina.png',
-            '/assets/desktop-icon-retina.png',
-            '/assets/phone-icon-blue.png',
-            '/assets/tablet-icon.png',
-            '/assets/desktop-icon.png',
             '/assets/check.png',
-            '/assets/check-retina.png',
             '/assets/off-focus.png',
-            '/assets/off-focus-retina.png',
-            '/assets/check-focus.png',
-            '/assets/check-focus-retina.png'
+            '/assets/check-focus.png'
         ]);
     });
-
-    /* form validation -------------------------------------------------------------- */
 
     // form validation http://jqueryvalidation.org/documentation/
     $(document).ready(function(){
         $('#contact').validate();
-	});
+    });
 
-    // Ajax form http://malsup.com/jquery/form/
+    // ajax form http://malsup.com/jquery/form/
     $(document).ready(function(){
         var options = {
             dataType: 'jsonp',
-			url: 'http://getsimpleform.com/messages/ajax?form_api_token=17732cc462230cce1807da0bed5d3bd9',
+            url: 'http://getsimpleform.com/messages/ajax?form_api_token=17732cc462230cce1807da0bed5d3bd9',
             target: '#contact-success', // target element(s) to be updated with server response
             beforeSubmit: showRequest,  // pre-submit callback
             success: showResponse,
             error: showAlert, // success callback
             timeout:   3000
-        }
+        };
+
         $('#contact').ajaxForm(options);
     });
 
@@ -178,21 +137,21 @@
 
     // post-submit callback
     function showResponse(responseText, statusText, xhr, $form) {
-	        $('#contact-processing').fadeOut(600, function() {
+            $('#contact-processing').fadeOut(600, function() {
             $('#contact-success').fadeIn(600);
         });
     }
 
      // post-submit callback
     function showAlert(responseText, statusText, xhr, $form) {
-	        $('#contact-processing').fadeOut(600, function() {
+            $('#contact-processing').fadeOut(600, function() {
             $('#contact-alert').fadeIn(600);
         });
     }
 
 })(window.jQuery);
 
-// set #deadline placeholder to 3 months in advance
+// set deadline placeholder text to 3 months ahead
 var today = new Date();
 var mm = today.getMonth()+4; //January is 0!
 var yyyy = today.getFullYear();
@@ -204,18 +163,18 @@ if(mm > 12){
 
 mm = mm.toString();
 
-if (mm == "1"){mm = "January"}
-if (mm == "2"){mm = "February"}
-if (mm == "3"){mm = "March"}
-if (mm == "4"){mm = "April"}
-if (mm == "5"){mm = "May"}
-if (mm == "6"){mm = "June"}
-if (mm == "7"){mm = "July"}
-if (mm == "8"){mm = "August"}
-if (mm == "9"){mm = "September"}
-if (mm == "10"){mm = "October"}
-if (mm == "11"){mm = "November"}
-if (mm == "12"){mm = "December"}
+if (mm == "1"){mm = "January";}
+if (mm == "2"){mm = "February";}
+if (mm == "3"){mm = "March";}
+if (mm == "4"){mm = "April";}
+if (mm == "5"){mm = "May";}
+if (mm == "6"){mm = "June";}
+if (mm == "7"){mm = "July";}
+if (mm == "8"){mm = "August";}
+if (mm == "9"){mm = "September";}
+if (mm == "10"){mm = "October";}
+if (mm == "11"){mm = "November";}
+if (mm == "12"){mm = "December";}
 
 var element = document.getElementById("Deadline");
 element.setAttribute("placeholder", "e.g. the end of " + mm + " " + yyyy);
