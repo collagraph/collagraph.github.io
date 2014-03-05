f34cc4f67845c8de65c21e7dafdafd836fcc140d
Robert Irish <robert.f.irish@gmail.com>
2014-03-05 18:47:05 +1100

    update development

--- _site/js/functions.js
+++ _site/js/functions.js
@@ -22,15 +22,15 @@
         }
     });
 
-	$('fieldset:not(#foo) #budget-q').click(function() {
-		if($('#budget-q').is(':checked')) {
-			$('#budget-c').removeClass('checkbox-focused').addClass('checkbox-checked-focused');
-			$('#more-budget').addClass('display-more').removeClass('display-hide');
-		} else {
-			$('#budget-c').removeClass('checkbox-checked, checkbox-checked-focused').addClass('checkbox-focused');
-			$('#more-budget').removeClass('display-more').addClass('display-hide');
-		}
-	});
+    $('fieldset:not(#foo) #budget-q').click(function() {
+        if($('#budget-q').is(':checked')) {
+            $('#budget-c').removeClass('checkbox-focused').addClass('checkbox-checked-focused');
+            $('#more-budget').addClass('display-more').removeClass('display-hide');
+        } else {
+            $('#budget-c').removeClass('checkbox-checked, checkbox-checked-focused').addClass('checkbox-focused');
+            $('#more-budget').removeClass('display-more').addClass('display-hide');
+        }
+    });
 
     $('fieldset:not(#foo) #phone-q').focus(function() {
         if ($('#phone-q').is(':checked')) {
@@ -74,44 +74,19 @@
         }
     });
 
-    // flexslider slide changer
-	$('.slide-1').click(function(){
-		var slidePath=$(this).closest('article').attr('id');
-		var slideURL='/assets/portfolio/'+slidePath+'/slide-1.jpg';
-        $('#'+slidePath+' .photo img').attr('src',slideURL);
-		$('#'+slidePath+' .slide-link').removeClass('current');
-		$(this).addClass('current');
-	});
-
-	$('.slide-2').click(function(){
-		var slidePath=$(this).closest('article').attr('id');
-		var slideURL='/assets/portfolio/'+slidePath+'/slide-2.jpg';
-		$('#'+slidePath+' .photo img').attr('src',slideURL);
-		$('#'+slidePath+' .slide-link').removeClass('current');
-		$(this).addClass('current');
-	});
-
-	$('.slide-3').click(function(){
-		var slidePath=$(this).closest('article').attr('id');
-		var slideURL='/assets/portfolio/'+slidePath+'/slide-3.jpg';
-        $('#'+slidePath+' .photo img').attr('src',slideURL);
-		$('#'+slidePath+' .slide-link').removeClass('current');
-		$(this).addClass('current');
-	});
-
     $(document).ready(function() {
 
-		// // flexslider
-		// $(".flexslider")
-		// .flexslider({
-		// 	animation: "slide",
-		// 	slideshow: false,
-		// 	useCSS: true,
-		// 	animationLoop: false,
-		// 	smoothHeight: false,
+        // // flexslider
+        // $(".flexslider")
+        // .flexslider({
+        //     animation: "slide",
+        //     slideshow: false,
+        //     useCSS: true,
+        //     animationLoop: false,
+        //     smoothHeight: false,
   //           directionNav: false,
-		// 	controlNav: false
-		// }).flexsliderManualDirectionControls();
+        //     controlNav: false
+        // }).flexsliderManualDirectionControls();
 
         // smooth scrolling
         $('.scroll').click(function(event){
@@ -131,14 +106,6 @@
 
     $(window).bind("load", function() {
         preload([
-            '/assets/portfolio/project-1/slide-2.jpg',
-            '/assets/portfolio/project-1/slide-3.jpg',
-            '/assets/phone-icon-blue-retina.png',
-            '/assets/tablet-icon-retina.png',
-            '/assets/desktop-icon-retina.png',
-            '/assets/phone-icon-blue.png',
-            '/assets/tablet-icon.png',
-            '/assets/desktop-icon.png',
             '/assets/check.png',
             '/assets/check-retina.png',
             '/assets/off-focus.png',
@@ -153,19 +120,20 @@
     // form validation http://jqueryvalidation.org/documentation/
     $(document).ready(function(){
         $('#contact').validate();
-	});
+    });
 
     // Ajax form http://malsup.com/jquery/form/
     $(document).ready(function(){
         var options = {
             dataType: 'jsonp',
-			url: 'http://getsimpleform.com/messages/ajax?form_api_token=17732cc462230cce1807da0bed5d3bd9',
+            url: 'http://getsimpleform.com/messages/ajax?form_api_token=17732cc462230cce1807da0bed5d3bd9',
             target: '#contact-success', // target element(s) to be updated with server response
             beforeSubmit: showRequest,  // pre-submit callback
             success: showResponse,
             error: showAlert, // success callback
             timeout:   3000
-        }
+        };
+
         $('#contact').ajaxForm(options);
     });
 
@@ -178,14 +146,14 @@
 
     // post-submit callback
     function showResponse(responseText, statusText, xhr, $form) {
-	        $('#contact-processing').fadeOut(600, function() {
+            $('#contact-processing').fadeOut(600, function() {
             $('#contact-success').fadeIn(600);
         });
     }
 
      // post-submit callback
     function showAlert(responseText, statusText, xhr, $form) {
-	        $('#contact-processing').fadeOut(600, function() {
+            $('#contact-processing').fadeOut(600, function() {
             $('#contact-alert').fadeIn(600);
         });
     }
@@ -204,18 +172,18 @@ if(mm > 12){
 
 mm = mm.toString();
 
-if (mm == "1"){mm = "January"}
-if (mm == "2"){mm = "February"}
-if (mm == "3"){mm = "March"}
-if (mm == "4"){mm = "April"}
-if (mm == "5"){mm = "May"}
-if (mm == "6"){mm = "June"}
-if (mm == "7"){mm = "July"}
-if (mm == "8"){mm = "August"}
-if (mm == "9"){mm = "September"}
-if (mm == "10"){mm = "October"}
-if (mm == "11"){mm = "November"}
-if (mm == "12"){mm = "December"}
+if (mm == "1"){mm = "January";}
+if (mm == "2"){mm = "February";}
+if (mm == "3"){mm = "March";}
+if (mm == "4"){mm = "April";}
+if (mm == "5"){mm = "May";}
+if (mm == "6"){mm = "June";}
+if (mm == "7"){mm = "July";}
+if (mm == "8"){mm = "August";}
+if (mm == "9"){mm = "September";}
+if (mm == "10"){mm = "October";}
+if (mm == "11"){mm = "November";}
+if (mm == "12"){mm = "December";}
 
 var element = document.getElementById("Deadline");
 element.setAttribute("placeholder", "e.g. the end of " + mm + " " + yyyy);
